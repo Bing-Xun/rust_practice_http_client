54045daec52cf1b7aad9425c7cb3ea1667d941f1
bing <wu20022002a@gmail.com>
2024-03-13 23:23:43 +0800

    re

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,18 @@
-use tokio::time::{sleep, Duration};
-
-async fn task1() {
-    println!("Task 1 started");
-    sleep(Duration::from_secs(5)).await;
-    println!("Task 1 completed");
-}
-
-async fn task2() {
-    println!("Task 2 started");
-    sleep(Duration::from_secs(1)).await;
-    println!("Task 2 completed");
-}
-
-async fn main_task() {
-    println!("Main task started");
-
-    // 启动异步任务，并交错执行
-    let join_handle1 = tokio::spawn(task1());
-    let join_handle2 = tokio::spawn(task2());
-
-    println!("##### ##### #####");
-
-    // 等待任务完成
-    println!("## 1");
-    // join_handle1.await.unwrap();
-    println!("## 2");
-    join_handle2.await.unwrap();
-    println!("## 3");
-
-    println!("Main task completed");
-}
+use reqwest::Error;
 
 #[tokio::main]
-async fn main() {
-    main_task().await;
+async fn main() -> Result<(), Error> {
+    // 发送 GET 请求
+    let response = reqwest::get("http://127.0.0.1:8080").await?;
+
+    // 检查响应状态码
+    if response.status().is_success() {
+        // 获取响应的文本内容并打印
+        let body = response.text().await?;
+        println!("Response body: {}", body);
+    } else {
+        println!("Request failed with status code: {}", response.status());
+    }
+
+    Ok(())
 }
